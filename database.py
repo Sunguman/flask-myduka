74dbdd107e4cab6aea19762391a09f5c4272f1fc
Thomas <thomas.sunguti2@gmail.com>
2026-07-10 19:01:28 +0300

    new code

--- database.py
+++ database.py
@@ -13,9 +13,38 @@ def get_products():
 
 def insert_products():
     cur.execute("insert into products(name,buying_price,selling_price)values('shoes',2000,2500)")
-    conn.commit()
+    conn.commit() # commit your changes permanently in the database
 
 insert_products()
 
 products = get_products()
 print(products)
+
+def get_sales():
+    cur.execute("select * from sales")
+    sales = cur.fetchall()
+    return sales
+
+sales = get_sales()
+print(sales)
+
+def insert_sales():
+    cur.execute("insert into sales(pid,quantity)values(2,5000)")
+    conn.commit()
+
+insert_sales()
+sales = get_sales()
+print(sales)
+
+def get_stock():
+    cur.execute("select * from stock")
+    stock = cur.fetchall()
+    return stock
+
+def insert_stock():
+    cur.execute("insert into stock(pid,stock_quantity)values(1,10)")
+    conn.commit()
+
+insert_stock()
+stock = get_stock()
+print(stock)
