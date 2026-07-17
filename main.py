7186b8379052f44c5603dfa98a27890072af4207
Thomas <thomas.sunguti2@gmail.com>
2026-07-17 17:23:10 +0300

    flask inheritance

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 # http://127.0.0.1:5000/products
-
 @app.route('/') #decorator function
 def home(): #view function
     return render_template('index.html')
